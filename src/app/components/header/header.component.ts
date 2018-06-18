1415af601fc282a3b95600554a5ad89ad2c6b5ff
david <david11sone@gmail.com>
2018-06-18 15:58:06 +0530

    dsada

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   openClose: string = 'open';
+  search:string;
   constructor() { }
 
   ngOnInit() {
