c49b756617cebf26156c7ae0bf9d5cf9a2c2d012
david <david11sone@gmail.com>
2018-06-18 17:46:03 +0530

    dsadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
-
 import { AppRoutingModule } from './app.routes';
 import { PageNotFoundComponent } from './components/page-not-found.component';
-
 import { AuthModule } from './auth.module';
-
-
 
 
 @Component({
