c29b9d8acc205b4988a3bc2f40f9a42a6355334a
david <david11sone@gmail.com>
2018-06-18 15:53:30 +0530

    sadasd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule,NoopAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { UiSwitchModule } from 'ngx-toggle-switch';
+
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
@@ -25,7 +25,6 @@ import { FooterComponent } from './components/footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    UiSwitchModule,
     BrowserAnimationsModule,
     NoopAnimationsModule
   ],
