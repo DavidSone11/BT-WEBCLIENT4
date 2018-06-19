816feaeda9cef837ba5ec6201a9a653589f21e9b
david <david11sone@gmail.com>
2018-06-19 16:42:07 +0530

    dasda

--- src/app/components/left-sidebar/left-sidebar.routing.ts
+++ src/app/components/left-sidebar/left-sidebar.routing.ts
@@ -10,7 +10,8 @@ const routes: Routes = [{
         { path: 'dashboard', loadChildren: '../dashboard/dashboard.module#DashboardModule', canActivate: [AuthGuard] },
         { path: 'train', loadChildren: '../train/train.module#TrainModule', canActivate: [AuthGuard] },
         { path: 'trainstation', loadChildren: '../train-station/train-station.module#trainStationModule' },
-        { path: 'blank', loadChildren: '..//blank/blank.module#blankModule' },
+        { path: 'blank', loadChildren: '../blank/blank.module#blankModule' },
+        { path: 'blank', loadChildren: '../home/home.module#HomeModule' },
         { path: 'userPlan', loadChildren: '../userplan/userplan.module#UserplanModule' },
         { path: 'user', loadChildren: '../user/user.module#UserModule' }
 
