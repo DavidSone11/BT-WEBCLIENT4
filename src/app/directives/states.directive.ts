2888656016013ccf61c0f9ed93a83867c67e1ece
david <david11sone@gmail.com>
2018-06-19 19:06:01 +0530

    dasdsa

--- src/app/directives/states.directive.ts
+++ src/app/directives/states.directive.ts
@@ -1,10 +1,14 @@
-import { Directive } from '@angular/core';
+
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appStates]'
 })
 export class StatesDirective {
 
-  constructor() { }
+  constructor(Element: ElementRef) {
+    console.log(Element);
+    Element.nativeElement.innerText = "Text is changed by changeText Directive. ";
+  }
 
 }
