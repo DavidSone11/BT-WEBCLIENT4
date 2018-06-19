3df8e8d69aa75f63a60cda3bc5a4731c3859d4fb
david <david11sone@gmail.com>
2018-06-19 20:14:43 +0530

    dasda

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { LeftSidebarComponent } from '../left-sidebar/left-sidebar.component';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  animations: [
+    trigger('headerAnim', [
+      state('active', style(
+        {
+          marginLeft: '0px',
+          transition: '0.8s all'
+        },
+
+
+      )),
+      state('inactive', style({
+        marginLeft: '-200px',
+        transition: '0.8s all'
+      })),
+      //transition('inactive => active', animate('2s all')),
+      //transition('active => inactive', animate('2s all'))
+    ])
+  ]
 })
 export class HeaderComponent implements OnInit {
 
@@ -12,12 +37,17 @@ export class HeaderComponent implements OnInit {
   search: string;
   constructor(private leftSidebarComponent: LeftSidebarComponent) { }
 
+
+  openClose: string = 'active';
+
+
   ngOnInit() {
 
   }
 
   openCloseSidebar(): void {
-  this.leftSidebarComponent.toggleSidebar();
+    this.leftSidebarComponent.toggleSidebar();
+    this.openClose = (this.openClose === 'active') ? 'inactive' : 'active';
 
   }
 
--- src/app/components/left-sidebar/left-sidebar.component.ts
+++ src/app/components/left-sidebar/left-sidebar.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, state, style } from '@angular/animations';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-left-sidebar',
@@ -7,22 +13,50 @@ import { trigger, state, style } from '@angular/animations';
   styleUrls: ['./left-sidebar.component.css'],
   animations: [
     trigger('sideMenuAnim', [
-      state('open', style({ marginLeft: '0px' })),
-      state('close', style({ marginLeft: '-200px' })),
+      state('active', style(
+        {
+          marginLeft: '0px',
+          transition: '0.8s all'
+        },
+
+
+      )),
+      state('inactive', style({ 
+        marginLeft: '-200px',
+        transition:'0.8s all'  
+      })),
+      //transition('inactive => active', animate('2s all')),
+      //transition('active => inactive', animate('2s all'))
+    ]),
+    trigger('contentAnimmation', [
+      state('active', style(
+        {
+          margin: '0px',
+          transition: '0.8s all'
+        },
+
+
+      )),
+      state('inactive', style({ 
+        margin: '-200px',
+        transition:'0.8s all'  
+      })),
+      //transition('inactive => active', animate('2s all')),
+      //transition('active => inactive', animate('2s all'))
     ])
   ]
 })
 export class LeftSidebarComponent implements OnInit {
 
-  openClose:string = 'open';
+  openClose: string = 'active';
   constructor() { }
 
   ngOnInit() {
   }
-   toggleSidebar():void {
+  toggleSidebar(): void {
 
-    this.openClose = (this.openClose==='open') ? 'close':'open';
+    this.openClose = (this.openClose === 'active') ? 'inactive' : 'active';
 
-   }
+  }
 
 }
