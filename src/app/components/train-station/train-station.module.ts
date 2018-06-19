a8c6c821fcdf9877d419a2d4f834ea9d3f72598a
david <david11sone@gmail.com>
2018-06-19 16:23:37 +0530

    cadasda

--- src/app/components/train-station/train-station.module.ts
+++ src/app/components/train-station/train-station.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TrainStationComponent } from './train-station.component';
 import { TrainStationRouting } from './train-station.routing';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   imports: [
     CommonModule,
     TrainStationRouting,
-    BrowserAnimationsModule
+
   ],
   declarations: [
     TrainStationComponent,
--- src/app/components/trains/train.module.ts
+++ src/app/components/trains/train.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TrainsComponent } from './trains.component';
 import { TrainsRouting } from './train.routing';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   imports: [
     CommonModule,
     TrainsRouting,
-    BrowserAnimationsModule
+    
   ],
   declarations: [
     TrainsComponent,
