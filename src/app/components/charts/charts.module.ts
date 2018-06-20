c5a9e7d2418f0e6dc54edb34f66ff8354cef1049
david <david11sone@gmail.com>
2018-06-20 23:03:24 +0530

    sdasd

--- src/app/components/charts/charts.module.ts
+++ src/app/components/charts/charts.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChartsComponent } from './charts.component';
 import { ChartsRouting } from './charts.routing';
-import { ChartsModule } from 'ng2-charts/ng2-charts';
+import { ChartsModule as ngChart } from 'ng2-charts/ng2-charts';
 
 @NgModule({
   imports: [
     CommonModule,
     ChartsRouting,
-    ChartsModule
+    ngChart
   
   ],
   declarations: [
