68296983a2304eff3379451aa389480e4df111fe
david <david11sone@gmail.com>
2018-06-21 18:53:41 +0530

    dasda

--- src/app/components/bootstrapComponents/tables/tables.module.ts
+++ src/app/components/bootstrapComponents/tables/tables.module.ts
@@ -3,10 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { TablesRoutingModule } from './tables-routing.module';
 import { TablesComponent } from './tables.component';
-import { PageHeaderModule } from './../../shared';
+
 
 @NgModule({
-    imports: [CommonModule, TablesRoutingModule, PageHeaderModule],
+    imports: [CommonModule, TablesRoutingModule],
     declarations: [TablesComponent]
 })
 export class TablesModule {}
