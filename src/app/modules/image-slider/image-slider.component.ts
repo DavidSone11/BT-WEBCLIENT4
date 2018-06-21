8ded00b3f3e5cb307d8836ed3f16dbf5c9151dd9
david <david11sone@gmail.com>
2018-06-21 15:54:34 +0530

    dsadas

--- src/app/modules/image-slider/image-slider.component.ts
+++ src/app/modules/image-slider/image-slider.component.ts
@@ -1,33 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-image-slider',
   templateUrl: './image-slider.component.html',
-  styleUrls: ['./image-slider.component.scss']
+  styleUrls: ['./image-slider.component.scss'],
+  providers: [NgbCarouselConfig]
 })
 export class ImageSliderComponent implements OnInit {
 
   public alerts: Array<any> = [];
   public sliders: Array<any> = [];
- 
-  constructor() {
+
+  constructor(config: NgbCarouselConfig, ) {
+    config.interval = 100;
+    config.wrap = false;
+    config.keyboard = false;
     this.sliders.push(
       {
         imagePath: 'assets/images/slider1.jpg',
-        label: 'First slide label',
-        text:
-          'Nulla vitae elit libero, a pharetra augue mollis interdum.'
+
       },
       {
         imagePath: 'assets/images/slider2.jpg',
-        label: 'Second slide label',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+
       },
       {
         imagePath: 'assets/images/slider3.jpg',
-        label: 'Third slide label',
-        text:
-          'Praesent commodo cursus magna, vel scelerisque nisl consectetur.'
+
+      },
+      {
+        imagePath: 'assets/images/adventure-air-aircraft-36487.jpg',
+
       }
     );
   }
