c667f78a7e8999320bfb49c74a439544f0b373e0
david <david11sone@gmail.com>
2018-06-22 13:03:53 +0530

    dsadas

--- src/app/components/userplan/userplan.component.ts
+++ src/app/components/userplan/userplan.component.ts
@@ -1,13 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { slideToTopTransition } from '../../router.animations';
+import {Router,ActivatedRoute} from '@angular/router'
+import "../../prototypes/format.prototype"
 
 @Component({
   selector: 'app-userplan',
   templateUrl: './userplan.component.html',
-  styleUrls: ['./userplan.component.scss']
+  styleUrls: ['./userplan.component.scss'],
+  animations: [slideToTopTransition()]
 })
 export class UserplanComponent implements OnInit {
 
-  constructor() { }
+  router:any;
+  urlpart:string;
+  constructor(private _router: Router,private activeRoute:ActivatedRoute,private http: HttpClient ) {
+    this.router = _router;
+    this.urlpart = this.router.url.checkRoute();
+
+
+  }
 
   ngOnInit() {
   }
--- src/app/components/userplan/userplan.module.ts
+++ src/app/components/userplan/userplan.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { UserplanComponent } from './userplan.component';
-
 import { UserPlanRouting } from './userplan.routing';
+import { PageHeaderModule } from '../../modules/index';
+import { BreadCrumbModule } from '../../modules/bread-crumb/breadCrumb.module';
+
 
 @NgModule({
   imports: [
     CommonModule,
-    UserPlanRouting
+    UserPlanRouting,
+    PageHeaderModule,
+    BreadCrumbModule
   ],
   declarations: [
     UserplanComponent
