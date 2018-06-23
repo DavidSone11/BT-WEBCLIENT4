a7614acf2fc6ddfa31bddb2f1db333a06bb220f4
david <david11sone@gmail.com>
2018-06-23 13:39:29 +0530

    dasda

---
+++ src/app/_classes/BaseSixtyFour.class.ts
@@ -0,0 +1,26 @@
+
+interface IBase {
+      keyStr: string;
+      encode(input: string): string;
+      decoding(): string;
+}
+
+export class BaseSixtyFour implements IBase {
+
+      keyStr: string = "santosh";
+
+      BaseSixtyFourClass() {
+
+      }
+      encode(input: string): string {
+            throw new Error("Method not implemented.");
+            console.log(btoa(this.keyStr));
+            return "";
+      }
+      decoding(): string {
+            throw new Error("Method not implemented.");
+            console.log(atob("eW91ciBlbmNvZGluZyB0ZXh0"));
+            return "";
+      }
+
+}
