2b23c5f7d582a56e5ec56fd2547a0fd90985498f
david <david11sone@gmail.com>
2018-06-23 14:08:56 +0530

    dsadas

---
+++ src/app/_services/login.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+
+import { Http } from '@angular/http';
+import { ILogin } from '../_interfaces/login.interface';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class LoginService implements ILogin {
+
+    isLoggedIn: boolean = false;
+
+    constructor(private _httpclient: HttpClient) {
+
+    }
+    checkSession(): boolean {
+        if (localStorage.getItem("token") && localStorage.getItem("username")) {
+            this.isLoggedIn = true;
+        } else {
+            this.isLoggedIn = false;
+            //   delete this.Username;
+        }
+        return (this.isLoggedIn) ? true : false;
+    }
+
+    doLogin(username, password): boolean {
+        throw new Error("Method not implemented.");
+    }
+    doLogout(): void {
+        throw new Error("Method not implemented.");
+        if (this.isLoggedIn) {
+            this.isLoggedIn = false;
+            localStorage.removeItem('username');
+            localStorage.removeItem('userplan');
+            localStorage.removeItem('token');
+            localStorage.removeItem('role');
+            localStorage.clear();
+        }
+    }
+
+
+}
