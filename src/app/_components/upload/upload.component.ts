2889d5bb3a8229e05876e517842417e61ae08389
david <david11sone@gmail.com>
2018-06-24 20:10:19 +0530

    dsad

--- src/app/_components/upload/upload.component.ts
+++ src/app/_components/upload/upload.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {Router,ActivatedRoute} from '@angular/router'
+import { Router, ActivatedRoute } from '@angular/router'
 import { HttpClient } from '@angular/common/http';
 import "../../_prototypes/format.prototype"
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-upload',
@@ -9,13 +10,15 @@ import "../../_prototypes/format.prototype"
   styleUrls: ['./upload.component.css']
 })
 export class UploadComponent implements OnInit {
-  
+
   commentsList: object;
   isloading: boolean = false;
+  arrayBuffer: any;
+  file: File;
 
-  router:any;
-  urlpart:string;
-  constructor(private _router: Router,private activeRoute:ActivatedRoute,private http: HttpClient ) {
+  router: any;
+  urlpart: string;
+  constructor(private _router: Router, private activeRoute: ActivatedRoute, private http: HttpClient) {
     this.router = _router;
     this.urlpart = this.router.url.checkRoute();
 
@@ -24,8 +27,25 @@ export class UploadComponent implements OnInit {
 
   ngOnInit() {
   }
-  
 
-  
+
+
+  incomingfile(event) {
+    this.file = event.target.files[0];
+    this.getUrl(event);
+  }
+
+  getUrl(event) {
+    let fileList: FileList = event.target.files;
+    if(fileList.length > 0) {
+        let file: File = fileList[0];
+        let formData:FormData = new FormData();
+        formData.append('uploadFile', file, file.name);
+      
+      
+    }
+}
+
+
 
 }
